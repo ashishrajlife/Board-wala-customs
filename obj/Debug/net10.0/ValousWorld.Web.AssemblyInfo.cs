fd45ab343ee871bbff13ec271d77158f48623c13
Ashish Raj <ashishrajlife@gmail.com>
2026-09-26 20:27:11 +0530

    updates

--- obj/Debug/net10.0/ValousWorld.Web.AssemblyInfo.cs
+++ obj/Debug/net10.0/ValousWorld.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ValousWorld.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e288d645d6d36e2f0ed633905633c9d8e0560d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7f8c5d09040c92380a8ea5be44354fe18cc80ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("ValousWorld.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ValousWorld.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
